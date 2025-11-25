d83c2faa5b5647aaf92237d7740eb092730cf0d8
GaioOliveira <gaio.oliveira3@etec.sp.gov.br>
2025-11-25 12:04:46 -0300

    Acesso ao MongoDB verificado

--- app.py
+++ app.py
@@ -14,34 +14,14 @@ try:
 except Exception as e:
     print(f"Erro ao conectar: {e}")
 
+# Rota principal 
 @app.route('/')
 def index():
     # Busca todos os itens (lanches e bebidas misturados)
     itens_banco = list(colecao_itens.find())
     return render_template('index.html', itens=itens_banco)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-app = Flask(__name__)
-
-try:
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client['hamburgueria_db']
-    # Podemos usar uma coleção genérica para todos os itens
-    colecao_itens = db['itens_cardapio'] 
-    print("Conectado ao MongoDB!")
-except Exception as e:
-    print(f"Erro ao conectar: {e}")
-
-@app.route('/')
-def index():
-    # Busca todos os itens (lanches e bebidas misturados)
-    itens_banco = list(colecao_itens.find())
-    return render_template('index.html', itens=itens_banco)
-
+# Rota para adicionar item ao cardápio
 @app.route('/adicionar', methods=['GET', 'POST'])
 def adicionar():
     if request.method == 'POST':
@@ -49,7 +29,7 @@ def adicionar():
         nome = request.form['nome']
         preco = float(request.form['preco'])
 
-        # Lógica de decisão simples (Factory Pattern simplificado)
+        # Decisão simples para verificar o tipo de item
         if tipo_item == 'lanche':
             ingredientes = request.form['ingredientes']
             novo_item = Lanche(nome, preco, ingredientes)
